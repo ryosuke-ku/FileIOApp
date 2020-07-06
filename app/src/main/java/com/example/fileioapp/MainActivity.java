cdb6171b62df6abaec40793c71160234dfb5bbff
ryosuke-ku <kurachi.ryosuke.kp0@gmail.com>
2020-07-06 20:00:43 +0900

    first commit

---
+++ app/src/main/java/com/example/fileioapp/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.fileioapp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private String fileName = "test.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+
+        editText = findViewById(R.id.edit_text);
+
+        Button buttonSave = findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                // エディットテキストのテキストを取得
+                String text = editText.getText().toString();
+
+                saveFile(fileName, text);
+                if(text.length() == 0){
+                    textView.setText(R.string.no_text);
+                }
+                else{
+                    textView.setText(R.string.saved);
+                }
+            }
+        });
+
+        Button buttonRead = findViewById(R.id.button_read);
+        buttonRead.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                String str = readFile(fileName);
+                if (str != null) {
+                    textView.setText(str);
+                } else {
+                    textView.setText(R.string.read_error);
+                }
+            }
+        });
+
+    }
+
+    // ファイルを保存
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void saveFile(String file, String str) {
+
+        // try-with-resources
+        try (FileOutputStream fileOutputstream = openFileOutput(file,
+                Context.MODE_PRIVATE);){
+
+            fileOutputstream.write(str.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // ファイルを読み出し
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String readFile(String file) {
+        String text = null;
+
+        // try-with-resources
+        try (FileInputStream fileInputStream = openFileInput(file);
+             BufferedReader reader= new BufferedReader(
+                     new InputStreamReader(fileInputStream, StandardCharsets.UTF_8))) {
+
+            String lineBuffer;
+            while( (lineBuffer = reader.readLine()) != null ) {
+                text = lineBuffer ;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
+}
